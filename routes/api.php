f5d0604aff5d5a338895a9abcabe4bd29aae8db1
Zenahr Barzani <zenmatica@gmail.com>
2020-06-05 21:38:31 +0200

    changed api endpoints to use implicit routing

--- routes/api.php
+++ routes/api.php
@@ -19,27 +19,8 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('items', function() {
-    return InventoryItem::all();
-});
-
-Route::get('items/{id}', function($id) {
-    return InventoryItem::find($id);
-});
-
-Route::post('items', function(Request $request) {
-    return InventoryItem::create($request->all);
-});
-
-Route::put('items/{id}', function(Request $request, $id) {
-    $InventoryItem = InventoryItem::findOrFail($id);
-    $InventoryItem->update($request->all());
-
-    return $InventoryItem;
-});
-
-Route::delete('items/{id}', function($id) {
-    InventoryItem::find($id)->delete();
-
-    return 204;
-});
+Route::get('items', 'InventoryItemController@index');
+Route::get('items/{item}', 'InventoryItemController@show');
+Route::post('items', 'InventoryItemController@store');
+Route::put('items/{item}', 'InventoryItemController@update');
+Route::delete('items/{item}', 'InventoryItemController@delete');
