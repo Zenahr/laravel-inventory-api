01c0ba2de7db1e02adcc9c68b87caa634a3f2458
Zenahr Barzani <zenmatica@gmail.com>
2020-06-05 22:33:40 +0200

    enhanced error handling

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -50,11 +50,14 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Throwable $exception)
     {
-        if ($exception instanceof ModelNotFoundException) {
-            return response()->json([
-                'error' => 'Resource not found'
-            ], 404);
-        }
+        if ($exception instanceof ModelNotFoundException &&
+        $request->wantsJson())
+    {
+        return response()->json([
+            'data' => 'Resource not found'
+        ], 404);
+    }
+
 
         return parent::render($request, $exception);
     }
